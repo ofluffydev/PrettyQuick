46a5ecd447a47a508d8171cb47edbe16315f293f
Kaden Frisk <fufferpish@gmail.com>
2024-09-08 20:15:20 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pretty Quick
+Simple powershell script to setup prettier based on [these docs](https://prettier.io/docs/en/install.html)
---
+++ setup_prettier.ps1
@@ -0,0 +1,3 @@
+yarn add --dev --exact prettier
+node --eval "fs.writeFileSync('.prettierrc','{}\n')"
+node --eval "fs.writeFileSync('.prettierignore','# Ignore artifacts:\nbuild\ncoverage\n')"
